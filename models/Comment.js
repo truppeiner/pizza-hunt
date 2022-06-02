5397d3c52c0ff074d52a50292d24dffd5c774ef6
Thomas Ruppeiner <truppeiner@gmail.com>
2022-06-02 16:47:29 -0500

    added replies

--- models/Comment.js
+++ models/Comment.js
@@ -1,4 +1,29 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const replySchema = new Schema ({
+    // set custom id to avoid confusion with parent comment_id
+    replyId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+    },
+    replyBody: {
+        type: String
+    },
+    writtenBy: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+    }
+},
+{
+    toJSON: {
+        getters: true
+    }
+});
 
 const CommentSchema = new Schema ({
     writtenBy: {
@@ -9,9 +34,25 @@ const CommentSchema = new Schema ({
     },
     createdAt: {
         type: Date,
-        default: Date.now
-    }
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+    },
+    replies: [
+        replySchema
+    ]
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
 });
+
+// get total count of replies on retrieval 
+CommentSchema.virtual('replyCount').get(function(){
+    return this.replies.length;
+  });
 
 const Comment = model('Comment', CommentSchema);
 
